063f48bdeb5e46b2d6d470d8505049a60b9a9761
yar83men <yarik83.evpat@gmail.com>
2025-05-08 14:50:44 +0300

    Initial commit

---
+++ src/test/java/org/yar_men/state_machine/StateMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yar_men.state_machine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StateMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
